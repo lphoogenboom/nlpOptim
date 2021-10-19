37162215570175e322dbc85ed1e16a4a6e5be02d
lphoogenboom <lphoogenboom@zelandlabs.com>
2021-10-19 14:53:10 +0200

    Puts A in vector

--- TaEv.m
+++ TaEv.m
@@ -6,13 +6,11 @@ function Ta = TaEv(x,u,vars,k)
     Ca = vars.Ca;
     Va = vars.Va;
     T_o = vars.V_o(k);
-    A1 = vars.A1;
-    A2 = vars.A2;
-    A3 = vars.A3;
+    A = vars.A;
     ha = vars.ha;
 
     mflow = mdot(u,x,vars,k);
     Ta = x(1)+ (dt)/(rhoa*Va*Ca) * (qdotp+mflow*Ca*(T_o-x(1)) + ...
-        ha*A1*(x(2)-x(1)) + ha*A2*(x(3)-x(1)) + ha*A3(x(4)-x(1)));
+        ha*A(1)*(x(2)-x(1)) + ha*A(2)*(x(3)-x(1)) + ha*A(3)*(x(4)-x(1)));
 end
 
