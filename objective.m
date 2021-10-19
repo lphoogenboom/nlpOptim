7a7bf8c5b32812d64ea4d0e27f24903f92ddc7dd
lphoogenboom <lphoogenboom@zelandlabs.com>
2021-10-19 15:18:44 +0200

    adds objective function func

--- objective.m
+++ objective.m
@@ -1,6 +1,8 @@
 function f = objective(x,u,v,vars,k)
-
-   
+    
+    Tref = vars.Tref;
+    Ca = vars.Ca;
+    dt = vars.dt;
     f = mdot(x,u,vars,k) * Ca * abs(x(1)-Tref*dt+(x(1)-Tref)^2);
 end
 
